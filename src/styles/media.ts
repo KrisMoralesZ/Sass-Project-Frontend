c8a2cfe95d1c5e1cee8479ac4aa66f3deecd30fb
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2026-08-04 09:46:34 -0700

    Fix format

--- src/styles/media.ts
+++ src/styles/media.ts
@@ -4,11 +4,17 @@ import { defaultTheme } from './theme'
 type Breakpoint = keyof AppTheme['breakpoints']
 
 /** Min-width media query for a theme breakpoint. */
-export function mediaUp(breakpoint: Breakpoint, theme: AppTheme = defaultTheme) {
+export function mediaUp(
+  breakpoint: Breakpoint,
+  theme: AppTheme = defaultTheme,
+) {
   return `@media (min-width: ${theme.breakpoints[breakpoint]})`
 }
 
 /** Max-width media query for a theme breakpoint. */
-export function mediaDown(breakpoint: Breakpoint, theme: AppTheme = defaultTheme) {
+export function mediaDown(
+  breakpoint: Breakpoint,
+  theme: AppTheme = defaultTheme,
+) {
   return `@media (max-width: calc(${theme.breakpoints[breakpoint]} - 0.02px))`
 }
