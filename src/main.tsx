15acb932045334397d5f5e12cd6d842d2670fcaf
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2026-08-27 12:43:58 -0700

    fix format

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import  AuthSessionProvider  from '@/features/auth/AuthSessionProvider'
-import  AppThemeProvider  from '@/styles/AppThemeProvider'
+import AuthSessionProvider from '@/features/auth/AuthSessionProvider'
+import AppThemeProvider from '@/styles/AppThemeProvider'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
