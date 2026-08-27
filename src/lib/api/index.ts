9a7b409f3bf75054e3861406dba56f405d33fb77
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2026-08-27 15:25:47 -0700

    Delete index

--- src/lib/api/index.ts
+++
@@ -1,2 +0,0 @@
-export { apiClient, http } from './api-client'
-export { ApiError, isApiError } from './api-error'
--- src/lib/index.ts
+++
@@ -1,4 +0,0 @@
-export { apiClient, ApiError, http, isApiError } from './api'
-export { AppQueryProvider } from './AppQueryProvider'
-export { getApiBaseUrl, getApiUrl } from './env'
-export { createQueryClient } from './query-client'
